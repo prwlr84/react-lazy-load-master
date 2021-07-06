8e751be48ebba890feb798d6469f32bc8c05eb77
Antal Bako <prowlersk8@gmail.com>
2021-07-06 14:18:15 +0200

    first

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+//import Artists from './Artists';
+import { Suspense, lazy } from 'react';
+
+const Artists = lazy(() => import('./Artists'));
+const Performers = lazy(() => import('./Performers'));
+
+class App extends React.Component {
+    render(){
+        return(
+            <div className="App">
+              <Suspense fallback={<h1>Still Loading…</h1>}>
+                <Artists />
+                <Performers />
+              </Suspense>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
